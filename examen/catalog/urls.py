c53837781e9d3287061050029c07235a044cea6b
Rubén Martínez Agramunt <rubecode11001@gmail.com>
2026-02-25 08:10:01 +0100

    Update urls.py

--- examen/catalog/urls.py
+++ examen/catalog/urls.py
@@ -26,13 +26,21 @@ urlpatterns += [
 urlconf examen 1ºev - catalog
 """
 
-urlpatterns += path('sacalibros/<int:pk>',views.editor_detail_view,name='sacalibros_editor'),
+urlpatterns += [
+    path('sacalibros/<int:pk>',views.editor_detail_view,name='sacalibros_editor'),
+]
 
-urlpatterns += path('books/<uuid:pk>/edit/',views.bookinstance_detail_view,name='bookinstance_detail'),
+urlpatterns += [
+    path('books/<uuid:pk>/edit/',views.bookinstance_detail_view,name='bookinstance_detail'),
+]
 
-urlpatterns += path('books/<uuid:pk>/',views.administrar_libros,name='administrar_libros_editor'),
+urlpatterns += [
+    path('books/<uuid:pk>/',views.administrar_libros,name='administrar_libros_editor'),
+]
 
-urlpatterns += path('eliminar/genero/<int:genero_pk>/libro/<uuid:bookinstance_pk>/',
+urlpatterns += [
+    path('eliminar/genero/<int:genero_pk>/libro/<uuid:bookinstance_pk>/',
                     views.eliminar_genero_libro,
                     name='eliminar_genero_libro'
                 ),
+]
